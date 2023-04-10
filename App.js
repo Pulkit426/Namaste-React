d78dea3f53597647c0c7efb5793b165d23258d7d
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2023-04-10 17:30:47 +0530

    episode 01

---
+++ App.js
@@ -0,0 +1,13 @@
+const div = React.createElement('div', {id: 'div1'}, 
+React.createElement('div', {id: 'div2'} , 
+[
+    React.createElement('h2', {id: 'heading'} , "Hello World from React"),
+    React.createElement('h1', {id: 'heading2'} , "Hello World from React")
+]
+)
+)
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(div)
