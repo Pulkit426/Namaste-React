aee802350874e107db388f87b0b40e32161d69ad
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2023-04-26 22:54:13 +0530

    add header component

--- App.js
+++ App.js
@@ -1,58 +1,31 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const jsxheading = <h1 id="heading"> Namaste React </h1>
 
-const HeadingComponent = () => {
+const Header = () => {
     return (
-        <div> 
-            <h1> Heading Component </h1>
-            {jsxheading}
+        <div className="header">
+            <img src="https://i.pinimg.com/originals/d2/82/c8/d282c8b0f4af7e8354081882ea4ae191.png" className="logo" />
+            
+            <nav className="nav-links">
+                <ul>
+                    <li> Home </li>
+                    <li> About </li>
+                    <li> Cart </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <div className="app">
+            <Header />
+
         </div>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<HeadingComponent />)
-
-
-
-
-
-
-//Assignment
-
-// const elem1 = React.createElement('div', {}, [React.createElement('h1', {}, 'H1'), React.createElement('h2',{}, 'H2' ), React.createElement('h3', {}, 'H3')] )
-
-
-// const elem2 = 
-// <div> 
-//     <h1> H1 JSX</h1>
-//     <h2> H2 JSX</h2>
-//     <h3> H3 JSX </h3>
-// </div>
-
-
-// const Elem3 = () => {
-//     return (
-//     <div>
-//         <h1> H1 FC</h1>
-//         <h2> H2 FC</h2>
-//         <h3> H3 FC</h3>
-//     </div>
-// )}
-
-
-
-
-// const Header = () => {
-//     return (
-//         <div id="header">
-//             <img src=''></img>
-//             <input type="text"></input> 
-//             <img src='' ></img>        
-//         </div>
-//     )
-// }
-
-
+root.render(<App />)
