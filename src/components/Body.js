e0381e5352db5fbf28783775b52f2399db835d14
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2023-05-02 23:38:44 +0530

    add search feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,9 +4,18 @@ import { useState } from "react"
 
 const Body = () => {
     const [listOfRestaurants, setListOfRestaurants] = useState(resList)
+    const [searchText, setSearchText] = useState('')
+
+    const filterDataBySearch = () => {
+        return resList.filter(res => res.data.name.includes(searchText))      
+    }
 
     return (
         <div className="body">
+            <div className="search">
+                <input type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+                <button onClick={() => { setListOfRestaurants(filterDataBySearch()) }}> Search </button>
+            </div>
             <div className="filter"> 
                 <button className="filter-btn" onClick={() => {
                     let filteredList = listOfRestaurants.filter(res => res.data.avgRating>4)
